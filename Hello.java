7e674a94e449385e2262bf20f14c8c39d3cf6c0d
leuco99 <111307298+leuco99@users.noreply.github.com>
2023-04-19 10:28:14 +0200

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -14,9 +14,30 @@ public class Hello {
         }
 
         int secondTopScore = 81;
-        if (topscore > secondTopScore && topscore < 100){
+        if ((topscore > secondTopScore) && (topscore < 100)){
             System.out.println("Greater than second top score and less than 100");
         }
 
+        if ((topscore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+        // == -> equal to
+        // != -> not equal to
+        // boolean isCar = false;
+        // if (isCar) {
+        // This tests if isCar is true
+
+        // boolean isCar = True;
+        // if (isCar) {
+        // This tests if isCar is true
+
+        //Ternary operator
+        // operand1 ? operand 2 : operand 3
     }
 }
